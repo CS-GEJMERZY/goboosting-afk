1b8234866603566527583ff8bc62f4d2fee9f3c2
Hacker <74736605+HackerDevs@users.noreply.github.com>
2024-07-15 18:37:22 +0200

    fix: player count

--- src/Models/ServerDataPayload.cs
+++ src/Models/ServerDataPayload.cs
@@ -3,8 +3,8 @@
 public class ServerDataPayload
 {
     public string Hostname { get; set; } = "invalid";
-    public int MaxPlayers { get; set; } = -1;
-    public int PlayerCount { get; set; } = -1;
+    public int MaxPlayers { get; set; } = 0;
+    public int PlayerCount { get; set; } = 0;
     public string MapName { get; set; } = "invalid";
     public bool Password { get; set; } = false;
     public string ModDesc { get; set; } = "Counter-Strike 2";
